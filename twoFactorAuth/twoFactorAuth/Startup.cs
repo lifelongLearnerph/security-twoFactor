d9559e21770d7956bab7d2c59b2f13d0eb42909a
hinep <hinep@BRSL3125>
2017-12-29 22:36:39 +0000

    Initial set up of projects

---
+++ twoFactorAuth/twoFactorAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(twoFactorAuth.Startup))]
+namespace twoFactorAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
